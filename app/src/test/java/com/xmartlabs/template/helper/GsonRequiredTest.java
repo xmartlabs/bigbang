45e7898358d8db653e08d1eb5f929d1adb928c81
Diego Medina <diego.medina@xmartlabs.com>
2017-04-03 11:00:37 -0300

    Exluded ModelAdapter in tests

--- app/src/test/java/com/xmartlabs/template/helper/GsonRequiredTest.java
+++ app/src/test/java/com/xmartlabs/template/helper/GsonRequiredTest.java
@@ -4,6 +4,7 @@ import com.google.gson.ExclusionStrategy;
 import com.google.gson.FieldAttributes;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.raizlabs.android.dbflow.structure.ModelAdapter;
 import com.xmartlabs.template.service.adapter.MillisecondsLocalDateAdapter;
 import com.xmartlabs.template.service.gson.GsonExclude;
 import com.xmartlabs.template.service.gson.GsonRequired;
@@ -30,7 +31,8 @@ public class GsonRequiredTest {
         .setExclusionStrategies(new ExclusionStrategy() {
           @Override
           public boolean shouldSkipField(FieldAttributes fieldAttributes) {
-            return fieldAttributes.getAnnotation(GsonExclude.class) != null;
+            return fieldAttributes.getAnnotation(GsonExclude.class) != null
+                || fieldAttributes.getDeclaredClass().equals(ModelAdapter.class);
           }
 
           @Override
