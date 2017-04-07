2fdf8f4e8cf3b66dd2f240108c45f97ef1dab016
Matías Irland <mirland@xmartlabs.com>
2017-04-07 16:48:44 -0300

    Pull request comments

--- app/src/main/java/com/xmartlabs/template/common/EntityProvider.java
+++ app/src/main/java/com/xmartlabs/template/common/EntityProvider.java
@@ -2,22 +2,22 @@ package com.xmartlabs.template.common;
 
 import com.annimon.stream.Optional;
 
-/** Used to provide an entity */
+/** Used to provide an entity. */
 public interface EntityProvider<T> {
   /**
-   * Provides the entity
+   * Provides the entity.
    *
    * @return the entity
    */
   Optional<T> provideEntity();
 
   /**
-   * Updates the entity in the database
+   * Updates the entity in the database.
    *
    * @param entity Entity to be saved
    */
   void updateEntity(T entity);
 
-  /** Deletes the entity in the database */
+  /** Deletes the entity in the database. */
   void deleteEntity();
 }
--- app/src/main/java/com/xmartlabs/template/providers/AccessTokenProvider.java
+++ app/src/main/java/com/xmartlabs/template/providers/AccessTokenProvider.java
@@ -6,12 +6,16 @@ import com.annimon.stream.Optional;
 import com.xmartlabs.template.common.EntityProvider;
 import com.xmartlabs.template.controller.SessionController;
 import com.xmartlabs.template.model.Session;
+import com.xmartlabs.template.module.SessionInterceptor;
 
 import javax.inject.Inject;
 
-/** Provides the access token in order to be added in the service requests */
+/**
+ * Provides the access token in order to be added in the service requests
+ * It's used by the {@link SessionInterceptor}
+ */
 public class AccessTokenProvider implements EntityProvider<String> {
-  private static final String AUTH_TOKEN_HEADER_KEY = "session"; // TODO 07/04/17: Add auth token header
+  private static final String AUTH_TOKEN_HEADER_KEY = "session";
 
   @NonNull
   private final SessionController sessionController;
@@ -28,6 +32,12 @@ public class AccessTokenProvider implements EntityProvider<String> {
         .flatMap(Optional::ofNullable);
   }
 
+  /**
+   * Provides the access token header key.
+   * This should be overridden to change the header key.
+   *
+   * @return the access token header key
+   */
   @NonNull
   public String provideAccessTokenHeaderKey() {
     return AUTH_TOKEN_HEADER_KEY;
