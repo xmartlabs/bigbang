b3de191f3b4c6bea2c1af69d2bb6c0953d711546
Mike <mike@xmartlabs.com>
2017-04-21 12:21:51 -0300

    Fixing dateadapter classes

--- core/src/main/java/com/xmartlabs/base/core/helper/gsonadapters/LocalDateAdapter.java
+++ core/src/main/java/com/xmartlabs/base/core/helper/gsonadapters/LocalDateAdapter.java
@@ -1,7 +1,5 @@
 package com.xmartlabs.base.core.helper.gsonadapters;
 
-import android.support.annotation.Nullable;
-
 import com.annimon.stream.Exceptional;
 import com.annimon.stream.Optional;
 import com.google.gson.Gson;
@@ -15,10 +13,7 @@ import com.xmartlabs.base.core.helper.StringUtils;
 
 import org.threeten.bp.Instant;
 import org.threeten.bp.LocalDate;
-import org.threeten.bp.LocalDateTime;
 import org.threeten.bp.ZoneOffset;
-import org.threeten.bp.ZonedDateTime;
-import org.threeten.bp.format.DateTimeFormatter;
 
 import java.lang.reflect.Type;
 import java.text.DateFormat;
@@ -26,20 +21,14 @@ import java.text.DateFormat;
 import lombok.RequiredArgsConstructor;
 import timber.log.Timber;
 
-<<<<<<< HEAD:core/src/main/java/com/xmartlabs/base/core/adapter/MillisecondsLocalDateAdapter.java
-/** {@link Gson} type adapter that serializes {@link LocalDate} objects to a millisecond format. */
-public class MillisecondsLocalDateAdapter implements JsonSerializer<LocalDate>, JsonDeserializer<LocalDate> {
-  @Nullable
-=======
 /** {@link Gson} type adapter that serializes {@link LocalDate} objects to any specified format. */
 @RequiredArgsConstructor
 public class LocalDateAdapter implements JsonSerializer<LocalDate>, JsonDeserializer<LocalDate> {
   final DateFormat dateFormat;
 
->>>>>>> 9df95cb... generifying Date time adapters:core/src/main/java/com/xmartlabs/base/core/helper/gsonadapters/LocalDateAdapter.java
   @Override
-  public synchronized JsonElement serialize(@Nullable LocalDate date, @Nullable Type type,
-                                            @Nullable JsonSerializationContext jsonSerializationContext) {
+  public synchronized JsonElement serialize(LocalDate date, Type type,
+                                            JsonSerializationContext jsonSerializationContext) {
     return Optional.ofNullable(date)
         .flatMap(localDate ->
             Exceptional.of(() ->
@@ -49,27 +38,14 @@ public class LocalDateAdapter implements JsonSerializer<LocalDate>, JsonDeserial
         .orElse(null);
   }
 
-  @Nullable
-  public synchronized JsonElement serialize(LocalDate date) {
-    return serialize(date, null, null);
-  }
-
-  @Nullable
   @Override
-  public synchronized LocalDate deserialize(@Nullable JsonElement jsonElement, @Nullable Type type,
-                                            @Nullable JsonDeserializationContext jsonDeserializationContext) {
-    return Optional.ofNullable(jsonElement)
-        .map(JsonElement::getAsString)
+  public synchronized LocalDate deserialize(JsonElement jsonElement, Type type,
+                                            JsonDeserializationContext jsonDeserializationContext) {
+    return Optional.ofNullable(jsonElement.getAsString())
         .filter(str -> !StringUtils.isNullOrEmpty(str))
-        .flatMap(str ->
-            Exceptional.of(() -> Instant.ofEpochMilli(Long.valueOf(str)).atZone(ZoneOffset.UTC).toLocalDate())
+        .flatMap(str -> Exceptional.of(() -> Instant.ofEpochMilli(Long.valueOf(str)).atZone(ZoneOffset.UTC).toLocalDate())
             .ifException(e -> Timber.e(e, "Date cannot be parsed, date='%s'", str))
             .getOptional())
         .orElse(null);
-  }
-
-  @Nullable
-  public synchronized LocalDate deserialize(@Nullable JsonElement jsonElement) {
-    return deserialize(jsonElement, null, null);
   }
 }
