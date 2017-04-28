6183ceeb0bdcd55f57c73a775ccf36d66f6a55b1
Mike <mike@xmartlabs.com>
2017-04-28 19:52:09 -0300

    Fixing updating libraries as a whole, not just one by one

--- uploadLibraries.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-./gradlew ui:bintrayUpload
-./gradlew core:bintrayUpload
-./gradlew dbflow:bintrayUpload
-./gradlew retrofit:bintrayUpload
-./gradlew log-crashlytics:bintrayUpload
