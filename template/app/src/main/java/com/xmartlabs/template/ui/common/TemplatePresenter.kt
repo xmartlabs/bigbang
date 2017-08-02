278ca82295f6dca529ce915bb2a4fdc2f3159506
Diego Medina <diego.medina@xmartlabs.com>
2017-08-02 11:21:16 -0300

    PR comments

--- template/app/src/main/java/com/xmartlabs/template/ui/common/TemplatePresenter.kt
+++ template/app/src/main/java/com/xmartlabs/template/ui/common/TemplatePresenter.kt
@@ -2,9 +2,6 @@ package com.xmartlabs.template.ui.common
 
 import com.xmartlabs.bigbang.ui.mvp.BaseMvpPresenter
 
-/**
- * Created by diegomedina24 on 27/7/17.
- */
 abstract class TemplatePresenter<T: TemplateView>: BaseMvpPresenter<T>() {
   override fun attachView(view: T) {
     super.attachView(view)
