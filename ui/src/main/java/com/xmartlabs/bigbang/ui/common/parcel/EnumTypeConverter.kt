bd1f9b1bb50eee0d3740ed66e07922f8ffcfa965
Matías Irland <matiasirland@gmail.com>
2017-08-16 10:45:19 -0300

    Add enum type converter

---
+++ ui/src/main/java/com/xmartlabs/bigbang/ui/common/parcel/EnumTypeConverter.kt
@@ -0,0 +1,25 @@
+package com.xmartlabs.bigbang.ui.common.parcel
+
+import org.parceler.ParcelConverter
+import java.util.*
+
+@Suppress("unused")
+open class EnumTypeConverter<T : Enum<T>>(val clazz: Class<T>) : ParcelConverter<T> {
+  override fun toParcel(input: T?, parcel: android.os.Parcel) {
+    if (input == null) {
+      parcel.writeInt(-1)
+    } else {
+      parcel.writeInt(1)
+      parcel.writeInt(input.ordinal)
+    }
+  }
+
+  override fun fromParcel(parcel: android.os.Parcel): T? {
+    if (parcel.readInt() == -1) {
+      return null
+    }
+    return EnumSet.allOf(clazz)
+        .filter { it.ordinal == parcel.readInt() }
+        .firstOrNull()
+  }
+}
