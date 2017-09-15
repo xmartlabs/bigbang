e894943a3139818739a18f3c53ee5323809289fd
Matías Irland <matiasirland@gmail.com>
2017-09-15 15:21:15 -0300

    Migrate some view extensions

---
+++ ui/src/main/java/com/xmartlabs/bigbang/ui/extension/ActivityExtensions.kt
@@ -0,0 +1,22 @@
+package com.xmartlabs.bigbang.ui.extension
+
+import android.app.Activity
+import android.content.Context
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import android.view.inputmethod.InputMethodManager
+
+/** Hides the keyboard, if visible.  */
+fun Activity.hideKeyboard() {
+  val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+  currentFocus?.let { inputMethodManager.hideSoftInputFromWindow(it.windowToken, 0) }
+}
+
+/**
+ * Removes the given fragment from the view.
+ *
+ * @param fragment the fragment to be removed
+ */
+fun AppCompatActivity.removeFragment(fragment: Fragment) {
+  supportFragmentManager.beginTransaction().remove(fragment).commit()
+}
